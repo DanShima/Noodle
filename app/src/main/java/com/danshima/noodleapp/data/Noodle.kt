00c63cc12e0a7bfb4841c5ccb4f99d50c6481a33
Danshima <giddy_with_panda@hotmail.com>
2019-01-06 22:07:21 +0100

    chore: prepare data class for Room

---
+++ app/src/main/java/com/danshima/noodleapp/data/Noodle.kt
@@ -0,0 +1,20 @@
+package com.danshima.noodleapp.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * The noodle class defines a single noodle dish with its attributes
+ * Created by Giddy on 04/11/2017.
+ */
+@Entity(tableName = "noodles")
+data class Noodle(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val description: String,
+    val photoID: Int,
+    val suggestedRestaurant: String,
+    val categoryNumber: Int)
+
